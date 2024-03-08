be762d41b65c595b6cf74b3e3368c44ba46dd7ef
Obiamaka Maria <obiamaka0101@gmail.com>
2024-03-08 17:40:42 +0000

    0x00. Pascal's Triangle Mandatory Task 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,18 +8,18 @@ def pascal_triangle(input_num):
     """
         Generates/Computes levels of pascal's triangle into list of lists
                                                                         """
-    trackArray = [] # Keeps track of current list generated
+    trackArray = []  # Keeps track of current list generated
     pascalArray = []
 
     for i in range(input_num):
         if len(trackArray) == 0:
-            trackArray.append(1) # Append 1 if array is empty
+            trackArray.append(1)  # Append 1 if array is empty
         else:
             temp = []
             for j in range(len(trackArray)):
                 if j == 0:
                     temp.append(trackArray[j])
-                if j + 1 >= len(trackArray): # Append 1 if last element 
+                if j + 1 >= len(trackArray):  # Append 1 if last element
                     temp.append(1)
                 else:
                     # Add current and next element
