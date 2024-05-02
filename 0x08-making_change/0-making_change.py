99875fdfe3d1e35d4b197dcf0b05ed8def7c138e
ObiamakaMaria <obiamaka0101@gmail.com>
2024-05-02 12:28:27 +0100

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,17 +10,17 @@ def makeChange(coins, total):
     """Returns the minimum number of coins needed to reach 'total' amount"""
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     fewest_coins = 0
-    
+
     for coin in coins:
         if total <= 0:
             break
         fewest_coins += total // coin
         total %= coin
-    
+
     if total != 0:
         return -1
-    
+
     return fewest_coins
