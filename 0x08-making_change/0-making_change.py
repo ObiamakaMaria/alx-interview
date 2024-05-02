3b4871ff4f0fc4c0928aa9c092f87cdec7992a01
Obiamaka Aghadiuno <125522368+ObiamakaMaria@users.noreply.github.com>
2024-05-02 12:10:58 +0100

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This function calculates the minimum number of coins required
+to achieve a specified total amount using a given set of coin values.
+"""
+
+
+def makeChange(coins, total):
+    """Returns the minimum number of coins needed to reach 'total' amount"""
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse=True)
+    fewest_coins = 0
+    
+    for coin in coins:
+        if total <= 0:
+            break
+        fewest_coins += total // coin
+        total %= coin
+    
+    if total != 0:
+        return -1
+    
+    return fewest_coins
