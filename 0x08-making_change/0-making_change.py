3885eb7249e4d4a2e3c112c37c9641c087a80d3a
ObiamakaMaria <obiamaka0101@gmail.com>
2024-05-02 11:55:56 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,31 +7,16 @@ to achieve a specified total amount using a given set of coin values.
 
 
 def makeChange(coins, total):
-    """Returns the minimum number of coins needed to reach 'total' amount"""
+    """return the fewest coins needed to meet total"""
     if total <= 0:
         return 0
-
     coins.sort(reverse=True)
-<<<<<<< HEAD
-    min_coins = 0
-    
-=======
-    fewest_coins = 0
-
->>>>>>> 99875fdfe3d1e35d4b197dcf0b05ed8def7c138e
+    min_req = 0
     for coin in coins:
         if total <= 0:
             break
-        min_coins += total // coin
+        min_req += total // coin
         total %= coin
-
     if total != 0:
         return -1
-<<<<<<< HEAD
-    
-    return min_coins
-=======
-
-    return fewest_coins
-
->>>>>>> 99875fdfe3d1e35d4b197dcf0b05ed8def7c138e
+    return min_req
